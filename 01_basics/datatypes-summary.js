48f2d79e709f5eda081041b12e5a4f6ccf9e9faa
Shekhar Suman <codewithshekhar9596@gmail.com>
2026-03-22 06:30:30 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,45 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symboll, BigInt
+
+const score = 100
+const scodeValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+// Symbol:-
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+// BigInt:-
+const bigNumber = 34876042748727623465n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions,
+// Array:-
+const heros = ["Shaktiman", "naagraj", "doga"]
+// Objects:-
+let myObj = {
+    name: "Shekhar",
+    age: 22,
+}
+// Function:-
+const myFunction = function () {
+    console.log("Hello world");
+}
+// TYpe of :-
+console.log(typeof bigNumber);
+
+// Type of Operator
+// Undefined => undefined
+// Null => object
+// Boolean => boolean
+// Number => Number
+// String => string
+// object(native and does not implement[[call]]) ===> object
+// object(native or host and does not implement[[call]]) ===> function
+// object(host and does not implement[[call]]) ===> implementation-defined except may not be "undefined","boolean","number",or"string
