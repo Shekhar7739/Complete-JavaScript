7d8e848445b37decf30706dc84f758d0c89e41d1
Shekhar Suman <codewithshekhar9596@gmail.com>
2026-03-24 09:40:32 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,45 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "Some@google.com",
+    fullname: {
+        userFullname: {
+            firstname: "Shekhar",
+            lastname: "Suman"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2}
+//const obj3 = Object.assign({} ,obj1,obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+//console.log(obj3);
+
+const users = [
+    {
+        
+    }
+]
+
+//users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
