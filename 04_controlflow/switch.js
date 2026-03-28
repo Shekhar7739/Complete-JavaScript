73e2aed4b0af531ce412c74a0ddb2abb27fd3c00
Shekhar Suman <codewithshekhar9596@gmail.com>
2026-03-28 15:44:02 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// const month = 12;
+const month = "march";
+
+switch (month) {
+    case "january":
+        console.log("January");
+        break;
+    case "february":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "june":
+        console.log("June");
+        break;
+    case "july":
+        console.log("July");
+        break;
+    case "august":
+        console.log("August");
+        break;
+    case "september":
+        console.log("September");
+        break;
+    case "october":
+        break;
+    case "november":
+        console.log("November");
+        break;
+    case "december":
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
+
