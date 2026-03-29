0345b76d948172e8fe481fdb759e737aff94d3af
Shekhar Suman <codewithshekhar9596@gmail.com>
2026-03-29 11:43:51 +0000

    two

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,3 +1,17 @@
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`This will run forever! ${index}`);
+//     index = index + 2;
+// }
 
-while (2 == 2) {
-    console.log("This will run forever!");
+let myarray = ["a", "b", "c", "d", "e"];
+let arr = 0;
+while (arr < myarray.length) {
+    // console.log(`Value is ${myarray[arr]}`);
+    arr = arr + 1;
+}
+let score = 11;
+do {
+    console.log(`Your score is ${score}`);
+    score = score + 1;
+} while (score <= 10);
